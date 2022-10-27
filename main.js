19dc3ccc2af97d7632712c9b7f83e17fef284279
Roman Mashin <roman.m@red-promo.ru>
2022-10-27 12:22:14 +0500

    add test

--- main.js
+++ main.js
@@ -44,5 +44,9 @@
   const getNotBtn = document.querySelector('#getNot');
   getNotBtn.addEventListener('click', () => {
     showNotification();
+    ServiceWorkerRegistration.showNotification('JavaScript Notification API', {
+      body: 'This is a JavaScript Notification API demo',
+      icon: './img/js.jpg',
+    });
   });
 })();
